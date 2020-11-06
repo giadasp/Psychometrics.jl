cd21c80d5c199490e91817dfd921c2f7bc6f424b
giadasp <giada.spaccapanico@gmail.com>
2020-11-06 18:50:35 +0100

    add item calibration via PG

---
+++ src/calibration/PolyaGamma_MCMC.jl
@@ -0,0 +1,7 @@
+function calibrate_item!(item::AbstractItem, responses::Vector{Response}, examinees::Vector{<:AbstractExaminee}; mcmc_iter = 4_000)
+    Distributed.@distributed @sinc for iter in 1:mcmc_iter 
+        W = generate_w(item, examinees)
+        mcmc_iter!(item, examinees, responses, pmap( y -> y.val, W); sampling = true)
+    end
+    update_estimate!(item)
+end
