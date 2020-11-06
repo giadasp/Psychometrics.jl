61883b2a3e1d980774fe7ea5436bd571bc43c3e2
giadasp <giada.spaccapanico@gmail.com>
2020-11-06 19:28:10 +0100

    fix bug

--- src/calibration/PolyaGamma_MCMC.jl
+++ src/calibration/PolyaGamma_MCMC.jl
@@ -1,5 +1,5 @@
 function calibrate_item!(item::AbstractItem, responses::Vector{Response}, examinees::Vector{<:AbstractExaminee}; mcmc_iter = 4_000)
-    Distributed.@distributed @sinc for iter in 1:mcmc_iter 
+    for iter in 1:mcmc_iter 
         W = generate_w(item, examinees)
         mcmc_iter!(item, examinees, responses, pmap( y -> y.val, W); sampling = true)
     end
