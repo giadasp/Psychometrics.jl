ab0584ebaf2ffb2e6fe59c53a56dbfd51c60a4fb
giadasp <giada.spaccapanico@gmail.com>
2020-12-03 02:06:41 +0100

    fix mcmc estimation methods

--- src/calibration/PolyaGamma_MCMC.jl
+++ src/calibration/PolyaGamma_MCMC.jl
@@ -6,18 +6,18 @@ function calibrate_item!(
     sampling = true,
 )
     empty_chain!(item)
-    chain = SharedArrays.SharedArray{Float64}(2,mcmc_iter)
-    # extract `mcmc_iter` samples from the polyagamma and from theta conditional posterior
-        Distributed.@sync Distributed.@distributed for iter in 1:mcmc_iter
-            W = generate_w(item, examinees)
-            chain[:,iter] = rand(posterior(item, examinees, responses, W))
-        end
-        # same as, but without val update (faster)
-        # for iter = 1:mcmc_iter
-        #    W = generate_w(item, examinees)
-        #    mcmc_iter!(item, examinees, responses, W; sampling = sampling)
-        # end
-    item.parameters.chain = [c[:] for c in eachcol(chain)]
+    # chain = SharedArrays.SharedArray{Float64}(2,mcmc_iter)
+    # # extract `mcmc_iter` samples from the polyagamma and from theta conditional posterior
+    # Distributed.@sync Distributed.@distributed for iter in 1:mcmc_iter
+    #     W = generate_w(item, examinees)
+    #     chain[:,iter] = rand(posterior(item, examinees, responses, W))
+    # end
+    # item.parameters.chain = [c[:] for c in eachcol(chain)]
+    # same as, but without val update (faster)
+    for iter = 1:mcmc_iter
+        W = generate_w(item, examinees)
+        mcmc_iter!(item, examinees, responses, W; sampling = sampling)
+    end
     update_estimate!(item; sampling = sampling)
 end
 
@@ -29,18 +29,19 @@ function estimate_ability!(
     sampling = true,
 )
     empty_chain!(examinee)
-    chain = SharedArrays.SharedArray{Float64}(mcmc_iter)
-    # extract `mcmc_iter` samples from the polyagamma and from theta conditional posterior
-        Distributed.@sync Distributed.@distributed for iter in 1:mcmc_iter
-            W = generate_w(examinee, items)
-            chain[iter] = rand(posterior(examinee, items, responses, W))
-        end
-        # same as, but without val update (faster)
-        # for iter = 1:mcmc_iter
-        #     W = generate_w(examinee, items)
-        #     mcmc_iter!(examinee, items, responses, W; sampling = sampling)
-        # end
-    examinee.latent.chain = copy(chain)
+    # chain = SharedArrays.SharedArray{Float64}(mcmc_iter)
+    # # extract `mcmc_iter` samples from the polyagamma and from theta conditional posterior
+    # Distributed.@sync Distributed.@distributed for iter in 1:mcmc_iter
+    #     W = generate_w(examinee, items)
+    #     chain[iter] = rand(posterior(examinee, items, responses, W))
+    # end
+    # examinee.latent.chain = copy(chain)
+
+    # same as, but without val update (faster)
+    for iter = 1:mcmc_iter
+        W = generate_w(examinee, items)
+        mcmc_iter!(examinee, items, responses, W; sampling = sampling)
+    end
     update_estimate!(examinee; sampling = sampling)
 end
 
