fa8a9943c07810bf45ede85e883cc9b387ac3e71
giadasp <giada.spaccapanico@gmail.com>
2021-01-18 22:02:52 +0100

    [docs] fix hidden pages 4th try

--- docs/make.jl
+++ docs/make.jl
@@ -8,12 +8,31 @@ makedocs(
     sitename = "Psychometrics",
     format = Documenter.HTML(),
     modules = [Psychometrics],
-    doctest = false,
+    doctest = true,
     
     pages = [
         "index.md",
-        "lib/public.md",
-        "lib/internals.md"   ]
+        hide("lib/public.md", [
+            "methods/exported/bayesian.md",
+            "methods/exported/distributions.md",
+            "methods/exported/examinee.md",
+            "methods/exported/information.md",
+            "methods/exported/likelihood.md",
+            "methods/exported/probability.md",
+            "methods/exported/response.md",
+            "methods/exported/item.md"
+        ]),
+        hide("lib/internals.md", [
+            "methods/internals/bayesian.md",
+            "methods/internals/distributions.md",
+            "methods/internals/information.md",
+            "methods/internals/latent.md",
+            "methods/internals/likelihood.md",
+            "methods/internals/probability.md",
+            "methods/internals/response.md",
+            "methods/internals/parameters.md"
+        ])
+    ]
     
 )
 
