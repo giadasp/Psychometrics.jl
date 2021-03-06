ef98497491875f16b442c8b9890f3099db99ae8e
giadasp <giada.spaccapanico@gmail.com>
2021-03-06 15:01:12 +0100

    fix bug in item factory

--- src/item/item.jl
+++ src/item/item.jl
@@ -21,14 +21,14 @@ An immutable containing information about an item (a question in a test), e.g. `
   - **`calibrated::Bool`**: Tells if the item has been already calibrated.
 
 # Factories
-    Item(idx::Int64, id::String, content::String, parameters::AbstractParameters) = new(idx, id, content, parameters)
-    Item(idx::Int64, id::String, parameters::AbstractParameters) = new(idx, id, "", parameters)
-    Item(idx::Int64, parameters::AbstractParameters) = new(idx, string(idx), "", parameters)
+    Item(idx::Int64, id::String, content::Vector{String}, parameters::AbstractParameters) = new(idx, id, content, parameters)
+    Item(idx::Int64, id::String, parameters::AbstractParameters) = new(idx, id, String[], parameters)
+    Item(idx::Int64, parameters::AbstractParameters) = new(idx, string(idx), String[], parameters)
 
 Creates a new generic item with custom index, id, (content features) and item parameters.
 
 # Random initilizers
-    Item(idx::Int64, id::String) = new(idx, id, "", Parameters1PL())
+    Item(idx::Int64, id::String) = new(idx, id, String[], Parameters1PL())
     Item(idx::Int64, id::String, content::String) = new(idx, id, content, Parameters1PL())
 
 Randomly generates a new generic calibrated item with custom index, id, content features and default 1PL item parameters 
@@ -41,12 +41,12 @@ struct Item <: AbstractItem
     parameters::AbstractParameters
 
     # Factories
-    Item(idx::Int64, id::String, content::String, parameters::AbstractParameters) = new(idx, id, content, parameters)
-    Item(idx::Int64, id::String, parameters::AbstractParameters) = new(idx, id, "", parameters)
-    Item(idx::Int64, parameters::AbstractParameters) = new(idx, string(idx), "", parameters)
+    Item(idx::Int64, id::String, content::Vector{String}, parameters::AbstractParameters) = new(idx, id, content, parameters)
+    Item(idx::Int64, id::String, parameters::AbstractParameters) = new(idx, id, String[], parameters)
+    Item(idx::Int64, parameters::AbstractParameters) = new(idx, string(idx), String[], parameters)
 
     # Random default initilizers
-    Item(idx::Int64, id::String) = new(idx, id, "", Parameters1PL())
+    Item(idx::Int64, id::String) = new(idx, id, String[], Parameters1PL())
     Item(idx::Int64, id::String, content::String) = new(idx, id, content, Parameters1PL())
 end
 
