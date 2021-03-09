18b0e39d82baaf90381838008140c319154b4de8
giadasp <giada.spaccapanico@gmail.com>
2021-03-09 16:50:55 +0100

    assess examinee only if assessed is false

--- src/examinee/assessment/mmle/mmle.jl
+++ src/examinee/assessment/mmle/mmle.jl
@@ -11,65 +11,65 @@ function assess_examinee_mmle!(
     x_tol_rel::Float64 = 0.001,
     f_tol_rel::Float64 = 0.00001,
     kwargs...)
+    if !examinee.assessed
+        #start points and probs
+        probs = Distributions.pdf(Distributions.Normal(metric[1], metric[2]), collect(-6.0:0.3:6.0))
+        probs = probs / sum(probs)
+        dist = Distributions.DiscreteNonParametric(collect(-6.0:0.3:6.0), probs)
 
-    #start points and probs
-    probs = Distributions.pdf(Distributions.Normal(metric[1], metric[2]), collect(-6.0:0.3:6.0))
-    probs = probs / sum(probs)
-    dist = Distributions.DiscreteNonParametric(collect(-6.0:0.3:6.0), probs)
-
-    #set priors 
-    set_prior!(examinees, dist);
-
-    #update posteriors
-    update_posterior!(examinees, items, responses; already_sorted = false);
-
-    #gr()
-    # starting values
-    before_time = time()
-    stop = false
-    old_likelihood = 0.0
-    old_pars = get_parameters_vals(items)
-    start_time = time()
-    response_matrix = get_response_matrix(responses, size(items,1), size(examinees,1));
-
-    iter = 1
-    while !stop
-        #rescale dist
-        rescale!(
-            dist,
-            examinees; 
-            metric = [0.0, 1.0]
-        )
-        #println("dist")
-        #display(plot(dist.support, dist.p))
-        #update examinees' support
-        map( e -> e.latent.prior = dist, examinees)
+        #set priors 
+        set_prior!(examinees, dist);
 
         #update posteriors
-        update_posterior!(examinees, items, response_matrix; already_sorted = false);
+        update_posterior!(examinees, items, responses; already_sorted = false);
 
-        if any([
-            check_iter(iter; max_iter = max_iter),
-            check_time(start_time; max_time = max_time),
-            check_f_tol_rel!(
-                examinees,
-                old_likelihood;
-                f_tol_rel = f_tol_rel
-                )
-            #TODO create method for exmainees 
-            # check_x_tol_rel!(
-            #     items,
-            #     old_pars;
-            #     x_tol_rel = x_tol_rel
-            # )
-            ])
-            stop = true
+        #gr()
+        # starting values
+        before_time = time()
+        stop = false
+        old_likelihood = 0.0
+        old_pars = get_parameters_vals(items)
+        start_time = time()
+        response_matrix = get_response_matrix(responses, size(items,1), size(examinees,1));
+
+        iter = 1
+        while !stop
+            #rescale dist
+            rescale!(
+                dist,
+                examinees; 
+                metric = [0.0, 1.0]
+            )
+            #println("dist")
+            #display(plot(dist.support, dist.p))
+            #update examinees' support
+            map( e -> e.latent.prior = dist, examinees)
+
+            #update posteriors
+            update_posterior!(examinees, items, response_matrix; already_sorted = false);
+
+            if any([
+                check_iter(iter; max_iter = max_iter),
+                check_time(start_time; max_time = max_time),
+                check_f_tol_rel!(
+                    examinees,
+                    old_likelihood;
+                    f_tol_rel = f_tol_rel
+                    )
+                #TODO create method for exmainees 
+                # check_x_tol_rel!(
+                #     items,
+                #     old_pars;
+                #     x_tol_rel = x_tol_rel
+                # )
+                ])
+                stop = true
+            end
+            iter += 1
+
         end
-        iter += 1
-
+        map( e -> e.latent.posterior = Distributions.DiscreteNonParametric(dist.support, e.latent.posterior.p), examinees);
+        map( e -> e.latent.val = e.latent.posterior.p' * e.latent.posterior.support, examinees);
     end
-    map( e -> e.latent.posterior = Distributions.DiscreteNonParametric(dist.support, e.latent.posterior.p), examinees);
-    map( e -> e.latent.val = e.latent.posterior.p' * e.latent.posterior.support, examinees);
-    
     return nothing
 end
